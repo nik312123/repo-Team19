804fa18a686cc59a145d24c03739f7ad0f93be95
Nikunj Chawla <nikchawla312@gmail.com>
2021-02-26 14:59:24 -0600

    Added base JavaDoc for VotingStreamParser

--- src/main/org/team19/VotingStreamParser.java
+++ src/main/org/team19/VotingStreamParser.java
@@ -1,3 +1,8 @@
 package org.team19;
 
+import java.io.InputStream;
+
+/**
+ * Parses an {@link InputStream} and returns a {@link VotingSystem}
+ */
 public class VotingStreamParser implements VotingStreamParserInterface {}
