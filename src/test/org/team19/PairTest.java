0e8b7c6e0564a6ed216c278d1490dd4f345862f1
Nikunj Chawla <nikchawla312@gmail.com>
2021-02-26 21:46:45 -0600

    Adjusted the pair tests to be more clear and useful

--- src/test/org/team19/PairTest.java
+++ src/test/org/team19/PairTest.java
@@ -3,49 +3,103 @@ package org.team19;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class PairTest {
+final class PairTest {
     
     private PairTest() {}
     
     @Test
     void testGetKey() {
+        final Object objInstance = new Object();
+        final Pair<Integer, Integer> twoFour = new Pair<>(2, 4);
+        final Pair<Character, Boolean> cFalse = new Pair<>('c', false);
+        final Pair<Integer, Boolean> twoFalse = new Pair<>(2, false);
+        final Pair<Object, Integer> objFour = new Pair<>(objInstance, 4);
+        final Pair<String, String> nullDemocrat = new Pair<>(null, "Democrat");
+        final Pair<String, Integer> obamaNull = new Pair<>("Obama", null);
+        
+        //Testing that keys are equivalennt to what was put into the constructor for the key
         Assertions.assertAll(
-            () -> Assertions.assertEquals(5, new Pair<>(5, 3).getKey()),
-            () -> Assertions.assertEquals("Hello", new Pair<>("Hello", 3).getKey()),
-            () -> Assertions.assertEquals('c', new Pair<>('c', "Hello").getKey()),
-            () -> Assertions.assertNull(new Pair<>(null, 3).getKey())
+            () -> Assertions.assertEquals(2, twoFour.getKey()),
+            () -> Assertions.assertEquals('c', cFalse.getKey()),
+            () -> Assertions.assertEquals(2, twoFalse.getKey()),
+            () -> Assertions.assertEquals(objInstance, objFour.getKey()),
+            //Special case: null key
+            () -> Assertions.assertNull(nullDemocrat.getKey()),
+            () -> Assertions.assertEquals("Obama", obamaNull.getKey())
         );
     }
     
     @Test
     void testGetValue() {
+        final Object objInstance = new Object();
+        final Pair<Integer, Integer> twoFour = new Pair<>(2, 4);
+        final Pair<Character, Boolean> cFalse = new Pair<>('c', false);
+        final Pair<Integer, Boolean> twoFalse = new Pair<>(2, false);
+        final Pair<Object, Integer> objFour = new Pair<>(objInstance, 4);
+        final Pair<String, String> nullDemocrat = new Pair<>(null, "Democrat");
+        final Pair<String, Integer> obamaNull = new Pair<>("Obama", null);
+        
+        //Testing that keys are equivalennt to what was put into the constructor for the value
         Assertions.assertAll(
-            () -> Assertions.assertEquals(5, new Pair<>(3, 5).getValue()),
-            () -> Assertions.assertNull(new Pair<>(3, null).getValue()),
-            () -> Assertions.assertEquals("Hello", new Pair<>(3, "Hello").getValue()),
-            () -> Assertions.assertEquals('c', new Pair<>("Hello", 'c').getValue())
+            () -> Assertions.assertEquals(4, twoFour.getValue()),
+            () -> Assertions.assertEquals(false, cFalse.getValue()),
+            () -> Assertions.assertEquals(false, twoFalse.getValue()),
+            () -> Assertions.assertEquals(4, objFour.getValue()),
+            () -> Assertions.assertEquals("Democrat", nullDemocrat.getValue()),
+            //Special case: null value
+            () -> Assertions.assertNull(obamaNull.getValue())
         );
     }
     
     @Test
     void testToString() {
+        final Object objInstance = new Object();
+        final Pair<Integer, Integer> twoFour = new Pair<>(2, 4);
+        final Pair<Character, Boolean> cFalse = new Pair<>('c', false);
+        final Pair<Integer, Boolean> twoFalse = new Pair<>(2, false);
+        final Pair<Object, Integer> objFour = new Pair<>(objInstance, 4);
+        final Pair<String, String> nullDemocrat = new Pair<>(null, "Democrat");
+        final Pair<String, Integer> obamaNull = new Pair<>("Obama", null);
+        
+        //Testing that toString prints the pairs in the format Pair{[key], [value]} where [key] and [value] are the pair's key and value, respectively
         Assertions.assertAll(
-            () -> Assertions.assertEquals("Pair{3, 5}", new Pair<>(3, 5).toString()),
-            () -> Assertions.assertEquals("Pair{3, null}", new Pair<>(3, null).toString()),
-            () -> Assertions.assertEquals("Pair{3, Hello}", new Pair<>(3, "Hello").toString()),
-            () -> Assertions.assertEquals("Pair{Hello, c}", new Pair<>("Hello", 'c').toString())
+            () -> Assertions.assertEquals("Pair{2, 4}", twoFour.toString()),
+            () -> Assertions.assertEquals("Pair{c, false}", cFalse.toString()),
+            () -> Assertions.assertEquals("Pair{2, false}", twoFalse.toString()),
+            () -> Assertions.assertEquals(String.format("Pair{%s, 4}", objInstance.toString()), objFour.toString()),
+            () -> Assertions.assertEquals("Pair{null, Democrat}", nullDemocrat.toString()),
+            () -> Assertions.assertEquals("Pair{Obama, null}", obamaNull.toString())
         );
     }
     
     @Test
     void testEquals() {
+        final Object objInstance = new Object();
+        final Pair<Integer, Integer> twoFour = new Pair<>(2, 4);
+        final Pair<Character, Boolean> cFalse = new Pair<>('c', false);
+        final Pair<Integer, Boolean> twoFalse = new Pair<>(2, false);
+        final Pair<Object, Integer> objFour = new Pair<>(objInstance, 4);
+        final Pair<String, String> nullDemocrat = new Pair<>(null, "Democrat");
+        final Pair<String, Integer> obamaNull = new Pair<>("Obama", null);
+        final Pair<Character, Boolean> cTrue = new Pair<>('c', true);
+        final Pair<Integer, Integer> threeFour = new Pair<>(3, 4);
+        
         Assertions.assertAll(
-            () -> Assertions.assertEquals(new Pair<>(3, 5), new Pair<>(3, 5)),
-            () -> Assertions.assertNotEquals(new Pair<>(3, 5), new Pair<>(3, 4)),
-            () -> Assertions.assertEquals(new Pair<>(3, 10), new Pair<>(3, (Number) 10)),
-            () -> Assertions.assertEquals(new Pair<>(5, 3), new Pair<>((Object) 5, 3)),
-            () -> Assertions.assertEquals(new Pair<>(null, 4), new Pair<>(null, 4)),
-            () -> Assertions.assertNotEquals(new Pair<>(new Pair<>(3, 5), null), new Pair<>(new Pair<>(3, 4), null))
+            //Testing pairs equivalent to the above variables (same input)
+            () -> Assertions.assertEquals(new Pair<>(2, 4), twoFour),
+            () -> Assertions.assertEquals(new Pair<>('c', false), cFalse),
+            () -> Assertions.assertEquals(new Pair<>(2, false), twoFalse),
+            () -> Assertions.assertEquals(new Pair<>(objInstance, 4), objFour),
+            () -> Assertions.assertEquals(new Pair<>(null, "Democrat"), nullDemocrat),
+            () -> Assertions.assertEquals(new Pair<>("Obama", null), obamaNull),
+            
+            //Testing that having equivalent keys but differing values does not result in equal pairs
+            () -> Assertions.assertNotEquals(twoFalse, twoFour),
+            () -> Assertions.assertNotEquals(cTrue, cFalse),
+            
+            //Testing that having equivalent values but differing keys does not result in equal pairs
+            () -> Assertions.assertNotEquals(cFalse, twoFalse),
+            () -> Assertions.assertNotEquals(twoFour, threeFour)
         );
     }
 }
