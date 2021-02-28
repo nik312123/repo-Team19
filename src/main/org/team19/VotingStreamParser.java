7fecede3081093b45e78ec699285a3e55c071906
Nikunj Chawla <nikchawla312@gmail.com>
2021-02-27 18:55:50 -0600

    Added audit stream and report stream to VotingStreamParser

--- src/main/org/team19/VotingStreamParser.java
+++ src/main/org/team19/VotingStreamParser.java
@@ -1,6 +1,7 @@
 package org.team19;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.ParseException;
 import java.util.Map;
 
@@ -13,11 +14,15 @@ public class VotingStreamParser {
      * Parses an {@link InputStream} and returns a {@link VotingSystem} constructed from the given stream
      *
      * @param input           The {@link InputStream} to parse
+     * @param auditStream     The {@link OutputStream} to write detailed information about the running of the election
+     * @param reportStream    The {@link OutputStream} to write a summary about the running of the election
      * @param headerSystemMap The mapping between header strings and their corresponding {@link VotingSystem} classes
      * @return The parsed {@link VotingSystem}
-     * @throws ParseException Thrown if there is an issue in parsing the provided {@link InputStream}
+     * @throws NullPointerException Thrown if any of the given streams or if the headerSystemMap is null
+     * @throws ParseException       Thrown if there is an issue in parsing the provided {@link InputStream}
      */
-    public VotingSystem parse(final InputStream input, final Map<String, Class<? extends VotingSystem>> headerSystemMap) throws ParseException {
+    public VotingSystem parse(final InputStream input, final OutputStream auditStream, final OutputStream reportStream,
+        final Map<String, Class<? extends VotingSystem>> headerSystemMap) throws ParseException, NullPointerException {
         return null;
     }
     
