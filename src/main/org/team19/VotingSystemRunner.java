5d1a46333aa0fdcb612b55ae5f53290f788340e0
Nikunj Chawla <nikchawla312@gmail.com>
2021-02-27 19:02:27 -0600

    Modified VotingSystemRunner to allow STDIN and have better file location finding

--- src/main/org/team19/VotingSystemRunner.java
+++ src/main/org/team19/VotingSystemRunner.java
@@ -1,6 +1,10 @@
 package org.team19;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Runs the election for a {@link VotingSystem} given a path to a file, which can be absolute or relative to the current working directory
@@ -23,17 +27,45 @@ public final class VotingSystemRunner {
      *             be absolute or relative to the current working directory
      */
     public static void main(final String[] args) {
-        if(args.length != 1) {
-            System.err.println(
-                "CompuVote requires exactly one command-line argument, which is an absolute file path or file path relative to repo-Team19 to the "
-                    + "election file to parse"
-            );
+        //The input stream from which to read input
+        final InputStream input;
+    
+        //If there are no arguments provided, then assume standard input is being used
+        if(args.length == 0) {
+            input = System.in;
+        }
+        //If there is one argument provided, then assume it is a file, and try to retrieve its input stream
+        else if(args.length == 1) {
+            String filePath = args[0];
+        
+            //If it starts with ~ plus the file separator, replace it with the home directory
+            if(filePath.startsWith("~" + File.separator)) {
+                filePath = System.getProperty("user.home") + filePath.substring(1);
+            }
+        
+            //Attempt to resolve the path name to a valid file path (system dependent)
+            try {
+                filePath = new File(filePath).getCanonicalPath();
+            }
+            catch(IOException e) {
+                System.err.println("The provided file path could not be resolved: " + e.getMessage());
+            }
+        
+            //Attempt to retrieve the file at the given file path
+            try {
+                input = new FileInputStream(filePath);
+            }
+            catch(FileNotFoundException e) {
+                System.err.println("The provided file could not be opened: " + e.getMessage());
+                System.exit(2);
+            }
+        }
+        //If there are more than one command-line arguments given, then print an error and exit with a nonzero status
+        else {
+            System.err.println("CompuVote can have 0 command-line arguments for standard input or 1 for a path to an election CSV file");
             System.exit(2);
         }
-        String filePath = args[0];
-        if(filePath.startsWith("~" + File.separator)) {
-            filePath = System.getProperty("user.home") + filePath.substring(1);
-        }
+    
     }
     
 }
