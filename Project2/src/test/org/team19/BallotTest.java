c2303f06443d4479eb74e0f94dd451899b7e29b0
Nikunj Chawla <chawl025@umn.edu>
2021-04-27 16:48:23 -0500

    Modified Ballot toString test to be more dynamic with the Candidate toString

--- Project2/src/test/org/team19/BallotTest.java
+++ Project2/src/test/org/team19/BallotTest.java
@@ -63,7 +63,10 @@ final class BallotTest {
             23,
             new Candidate[] {c0, c1}
         );
-        Assertions.assertEquals("Ballot{ballotNumber=23, candidateIndex=-1, rankedCandidates=[C0 (P0), C1 (P1)]}", ballot.toString());
+        Assertions.assertEquals(
+            String.format("Ballot{ballotNumber=23, candidateIndex=-1, rankedCandidates=[%s, %s]}", c0, c1),
+            ballot.toString()
+        );
     }
     
     @Test
